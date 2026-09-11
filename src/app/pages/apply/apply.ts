286a1fba4609321d617097846746e8a261a4c0bd
Shree Niwas Kushwah <shri.mca2010@gmail.com>
2026-09-11 23:51:43 +0530

    first commit

---
+++ src/app/pages/apply/apply.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-apply',
+  styleUrl: './apply.css',
+  templateUrl: './apply.html',
+})
+export class Apply {}
---
+++ src/app/pages/career/career.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-career',
+  styleUrl: './career.css',
+  templateUrl: './career.html',
+})
+export class Career {}
